72f7b0fa45ce0400ac9de10d5088f9a0b5a209c8
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-05 18:22:47 +0200

    Initial Commit

---
+++ PearsonsListSwiftUI/PearsonsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearsonsListSwiftUIApp.swift
+//  PearsonsListSwiftUI
+//
+//  Created by Vladimir Dvornikov on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PearsonsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
